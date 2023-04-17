0db401d215dc027105f61612c105eba6b79ff731
Phridge <rl.foerster.2002@gmail.com>
2023-04-17 19:47:50 +0200

    Interaktiv test

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
 import pyglet
+from reactivex.subject import BehaviorSubject
+
+stuff = BehaviorSubject(None)
 
 window = pyglet.window.Window()
 
-label = pyglet.text.Label('Hello, world',
+label = pyglet.text.Label("",
                           font_name='Times New Roman',
                           font_size=36,
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
+
+
+def reaction(x):
+    label.text = f"Pressed char {chr(x[0])}, modifiers {x[1]}" if x else "Press something"
+
+
+stuff.subscribe(reaction)
+
 # phil war hier
 # Martin war hier
 
@@ -14,6 +25,11 @@ label = pyglet.text.Label('Hello, world',
 def on_draw():
     window.clear()
     label.draw()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    stuff.on_next((symbol, modifiers))
+
 
 
 pyglet.app.run()
