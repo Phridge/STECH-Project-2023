96a76ecf7da9ccca8123809dec6fb00285923c11
Phridge <rl.foerster.2002@gmail.com>
2023-04-19 17:21:23 +0200

    Ui mit mehreren panels test

--- game.py
+++ game.py
@@ -7,34 +7,25 @@ from reactivex.operators import publish
 window = pyglet.window.Window(resizable=False)
 
 
-
-drawable = None
-
-
-def set_drawable(d):
-    assert hasattr(d, "draw")
-    global drawable
-    drawable = d
-
-
 @window.event
 def on_draw():
     window.clear()
-    if drawable:
-        drawable.draw()
+    view = controller.get_view()
+    view.draw()
 
 
 class events:
-    key_press = Subject()
+    key = Subject()
     text = Subject()
     mouse = BehaviorSubject((0, 0))
     mouse_move = Subject()
+    mouse_button = Subject()
     size = BehaviorSubject((window.width, window.height))
 
 
 @window.event
 def on_key_press(keycode, mods):
-    events.key_press.on_next((keycode, mods))
+    events.key.on_next((keycode, mods))
 
 
 @window.event
@@ -48,24 +39,37 @@ def on_mouse_motion(x, y, dx, dy):
     events.mouse.on_next((x, y))
 
 
+@window.event
+def on_mouse_down(x, y, button, modifiers):
+    events.mouse_button.on_next((True, x, y, button, modifiers))
+
+
+@window.event
+def on_mouse_up(x, y, button, modifiers):
+    events.mouse_button.on_next((False, x, y, button, modifiers))
+
 
 @window.event
 def on_resize(w, h):
     events.size.on_next((w, h))
 
 
-class TestController:
-    def __init__(self):
-        def chars():
-            import random
-            while True:
-                yield random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
-        self.char_iter = chars()
-        self.active_letter = next(self.char_iter)
+class Controller:
+    def get_view(self):
+        raise NotImplementedError
+
+
+class Controller1(Controller):
+    def __init__(self):
+        def rchar():
+            import random
+            return random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+        self.active_letter = rchar()
 
         self.batch = pyglet.graphics.Batch()
-        self.mouse_pos = pyglet.text.Label("", font_name="Arial", font_size=36, anchor_y="bottom", anchor_x="left", batch=self.batch)
+        self.mouse_pos = pyglet.text.Label(font_name="Arial", font_size=36, anchor_y="bottom", anchor_x="left", batch=self.batch)
         self.write_letter = pyglet.text.Label(self.active_letter, font_name="Arial", font_size=50, anchor_y="center", anchor_x="center", batch=self.batch)
 
         def mouse(data):
@@ -73,21 +77,69 @@ class TestController:
 
         def size(data):
             self.write_letter.x, self.write_letter.y = data[0] // 2, data[1] // 2
-            return
-            self.view.x, self.view.y = 0, 0
 
         def on_input(text):
             if self.active_letter == text:
-                self.write_letter.text = self.active_letter = next(self.char_iter)
+                self.write_letter.text = self.active_letter = rchar()
 
         events.mouse.subscribe(mouse)
         events.size.subscribe(size)
         events.text.subscribe(on_input)
 
-        set_drawable(self.batch)
+    def get_view(self):
+        return self.batch
 
 
-controller = TestController()
+class Controller2(Controller):
+
+    def __init__(self):
+        self.on_back = Subject()
+
+        self.batch = pyglet.graphics.Batch()
+
+        self.back_label = pyglet.text.Label("Press space to go back", batch=self.batch, font_name="Arial", font_size=36, anchor_x="center", anchor_y="center", x=window.width//2, y=window.height//2)
+
+        events.key.subscribe(self.key_press)
+
+    def key_press(self, data):
+        key, mods = data
+        if key == pyglet.window.key.SPACE:
+            self.on_back.on_next(None)
+
+    def get_view(self):
+        return self.batch
+
+
+class GameController(Controller):
+    def __init__(self):
+        self.current = None
+
+
+        self.batch = pyglet.graphics.Batch()
+
+        self.label = pyglet.text.Label("Press 1 or 2", batch=self.batch, font_name="Arial", font_size=36, anchor_x="center", anchor_y="center", x=window.width//2, y=window.height//2)
+
+        events.key.subscribe(self.key_press)
+
+
+    def key_press(self, data):
+        if self.current is not None:
+            return
+
+        key, mods = data
+
+        if key == pyglet.window.key._1:
+            self.current = Controller1()
+        elif key == pyglet.window.key._2:
+            self.current = Controller2()
+            self.current.on_back.subscribe(lambda _: setattr(self, "current", None))
+
+
+    def get_view(self):
+        return self.batch if self.current is None else self.current.get_view()
+
+
+controller = GameController()
 
 
 pyglet.app.run(1/30)
