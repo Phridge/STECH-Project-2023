59936345b626bfbe28a26f1729daa0ce0f3ae6a6
Phridge <rl.foerster.2002@gmail.com>
2023-05-28 13:27:51 +0200

    Level1 doku

--- controller/story_mode/level1_screen.py
+++ controller/story_mode/level1_screen.py
@@ -35,6 +35,16 @@ def linear(t):
 
 
 def animate(lo, hi, time, update_event, map=lambda x: x, interp=linear):
+    """
+    Animiert einen Wert von lo nach hi über gegebene Zeit.
+    :param lo: startwert.
+    :param hi: endwert.
+    :param time: zeitspanne der animation.
+    :param update_event: observable, welches das "timing" vorgibt (z.B. events.update)
+    :param map: optionale mapping-funktion, die den Animierten wert in etwas anderes konvertiert.
+    :param interp: Interpolierungsfunktion. zur verfügung steht aktuell "linear", welches gleichmäßig vom einen zum anderen wert animiert.
+    :return: Ein Observable, welches schrittweise den animierten wert liefert
+    """
     animation = update_event.pipe(
         scan(float.__add__, 0.0),
         take_while(lambda t: t <= time, inclusive=True),
@@ -70,36 +80,48 @@ Die Reise wird lang und herausfordernd, hoffentlich habe ich genug Ersatz-Teetas
         )
         style = Style(events.color_scheme, "Monocraft", 15)
         self.batch = pyglet.graphics.Batch()
+
         # dient, um Objekte manuell nach vorne und hinten zu schieben. Je weniger er genutzt wird, umso performanter ist alles.
         # Standardmäßig ist alles im Mittelgrund zwischen Vorder- und Hintergrund
         background = pyglet.graphics.Group(order=-1)
         foreground = pyglet.graphics.Group(order=1)
         overlay = pyglet.graphics.Group(order=2)
 
+        # hintergrund
         self.gif = ui_elements.Gif("assets/images/port.gif", 0, 0, 100, 100, 30, True, self.events, self.batch, background)
 
         # Player-Objekt
         # player = Player(self.events, self.batch, 20, 40, 20, 30)
 
 
+        # Spieler-Objekt. Bis is eine Position erhält, ist es nicht sichtbar.
         self.p = ThePlayer(batch=self.batch, group=foreground)
+        # Region, in dem sich der spieler bewegen darf. Die untere kante stellt den laufsteg dar.
         player_area = pos.pipe(
             map_inner_perc(0, 40, 100, 60)
         )
 
+        # event, welches von der Machine s. unten den anzuzeigenden Text erhält-
         text = Event()
 
+        # um die Spielereingaben Textübergreifend zu sammeln und in die Datenbank zu speicher.
         input_analysis = InputAnalysis()
         self.input_box = InputBox(text, pos.pipe(map_inner_perc(10, 5, 80, 20)), style, events, input_analysis, batch=self.batch, group=foreground)
+        # ist ein Text fertig geschrieben, wird die Machine um einen Schritt vorangebracht.
         self._subs.add(
             self.input_box.text_tracker.pipe(
                 rfilter(lambda tt: tt.is_finished)
             ).subscribe(lambda _: machine.next())
         )
 
+        # Überschrift.
         self.header = ui_elements.BorderedRectangle("Level 1: Der Hafen der Freiheit", 20, 80, 60, 20, self.events.color_scheme, color_scheme.Minecraft, 2, self.events, self.batch)
 
         class Machine:
+            """
+            Stellt den Ablauf des Levels in einer Finite State Machine dar.
+            Hat mehrere Schritte, die nacheinander abgearbeitet werden (durch machine.next()).
+            """
             def __init__(self, stuff):
                 self.stuff = stuff
                 self.index = -1
@@ -107,31 +129,56 @@ Die Reise wird lang und herausfordernd, hoffentlich habe ich genug Ersatz-Teetas
                 self.next()
 
             def next(self):
+                """
+                Die Maschine in den nächsten Zustand bringen
+
+                Die Ressourcen vom vorherigen Schritt, falls existierend, werden gelöscht, und die
+                Funktion des nächsten Zustands wird ausgeführt.
+                """
                 self.index += 1
                 if self.index >= len(self.stuff):
                     return
                 self.disposable.disposable = self.stuff[self.index]()
 
         def display_text(display_text):
+            """
+            Factory-Funktion, um einfach den nächsten Text in der Input-Box anzuzeigen.
+            :param display_text: Zu schreibender Text.
+            :return: eine Funktion, die von der Machine ausgeführt werden kann.
+            """
             return lambda: text.on_next(display_text)
 
         def player_entry():
+            """
+            Eröffnung des Levels.
+
+            Der Bildschirm faded von Schwarz zu durchsichtig, und der Spieler läuft von der Linken seite ein.
+            :return: Nachher zu löschenden Ressourcen.
+            """
+            # farbanimation
             color = animate(255, 0, 3.0, events.update, lambda o: (0, 0, 0, int(o)))
+            # spielerpositionsanimation.
+            # Sieht kompliziert aus, da noch die Position des Laufstegs mit berücksichtigt werden muss.
             player_pos = animate(-300, 100, 5.0, events.update).pipe(
-                do_action(on_completed=lambda: machine.next()),
+                do_action(on_completed=lambda: machine.next()),  # wenn die animation durch ist, nächster Zustand
                 combine_latest(player_area),
                 starmap(lambda xoff, area: Rect(xoff, 0, 150, 150).offset(area)),
             )
+            # Das Rechteck für den FAde
             overlay_rect = Rectangle(pos, color, self.batch, overlay)
 
+            # den Spieler zum Laufen bringen.
             self.p.running_speed.on_next(2.0)
             return CompositeDisposable(
                 overlay_rect,
                 player_pos.subscribe(self.p.pos),
-                Disposable(lambda: self.p.running_speed.on_next(0.0))
+                Disposable(lambda: self.p.running_speed.on_next(0.0))  # wenn fertig, dann spieler stoppen.
             )
 
         def player_exit():
+            """
+            Wie player_entry, nur umgekehrt.
+            """
             # save game
             save_run(save, "story_level_1", input_analysis)
 
@@ -150,16 +197,15 @@ Die Reise wird lang und herausfordernd, hoffentlich habe ich genug Ersatz-Teetas
                 player_pos.subscribe(self.p.pos),
             )
 
+        # Erstellung der State Machine aus allen nötigen Zuständen:
+        # Spieler entry
+        # dann die ganzen texte
+        # dann Spieler exit
         machine = Machine(
             [player_entry]
             + [display_text(text) for text in self.TEXTS]
             + [player_exit]
         )
-
-        interactions = Event()
-        self._subs.add(interactions.subscribe(lambda _: machine.next()))
-
-        # self.test = pyglet.shapes.Arc(100, 100, 50, angle=math.pi, closed=True, batch=self.batch, group=foreground)
 
 
     def get_view(self):  # Erzeugt den aktuellen View
