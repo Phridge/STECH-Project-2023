402afb549415d27535181fb19877e73995b6c0fc
Phridge <rl.foerster.2002@gmail.com>
2023-06-03 13:29:12 +0200

    LevelMachine

--- controller/story_mode/__init__.py
+++ controller/story_mode/__init__.py
@@ -1,3 +1,5 @@
+from typing import Callable, Any, Generator
+
 from pyglet.graphics import Group
 from reactivex import Observable
 from reactivex.abc import DisposableBase
@@ -35,7 +37,46 @@ class Machine(DisposableBase):
         self.disposable.dispose()
 
 
+class LevelMachine:
+    """
+    Eine State Machine für Level, welche über python-Generator-Funktionen implementiert ist.
+
+    Für Beispiel s. Level2 Screen.
+    """
+    def __init__(self, generator_func: Callable[[Any], Generator]):
+        """
+        Initialisiere eine LevelMachine.
+
+        :param generator_func: eine Funktion, die einen Generator kreiert (also eine funktion, die yield statements
+        enthält) und EIN Argument annimmt. Dieses eine Argument wird eine Funktion sein, die aus dem Generator aufgerufen
+        werden kann, um ausführung des Generators fortzufahren.
+        """
+        self.machine_disposable = SerialDisposable()
+
+        self.generator = generator_func(self.switch_state)
+
+        self.switch_state(None)
+
+    def switch_state(self, data=None):
+        """
+        Gehe in den nächsten Zustand über.
+        :param data: Daten, die an den Generator gesendet werden.
+        :return: nichts
+        """
+        self.machine_disposable.disposable = None
+        try:
+            self.machine_disposable.disposable = self.generator.send(data)
+        except StopIteration:
+            pass
+
+
 class Level(Screen):
+    """
+    Superklasse für Storylevel.
+
+    Stellt bereit:
+    * mehrere gruppen zum reinrendern, gerne auch untergruppen für diese erstellen wenn nötig
+    """
     def __init__(self):
         super().__init__()
 
