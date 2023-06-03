c61f00b4c2051d63321eec8a19634ba463f34ed8
Kirizuke <unknown.or.remember@gmail.com>
2023-06-04 00:08:10 +0200

    Statistic vie ist eingebunden und funktioniert

--- controller/statistics.py
+++ controller/statistics.py
@@ -3,31 +3,79 @@ import color_scheme
 import ui_elements
 from reactivex.subject import Subject
 from reactivex.disposable import CompositeDisposable
-
+import statistics.statistic_view as sv
+import statistics.statistics_controller as sc
 from controller import Screen
 
 
 class StatisticsScreen(Screen):
     def __init__(self, events, save):
         super().__init__()
-        if save == 0: save_text = "Alle Spielstände"
-        else: save_text = "Spielstand " + str(save)
-
+        if save == 0:
+            save_text = "Alle Spielstände"
+        else:
+            save_text = "Spielstand " + str(save)
+        self.game_save = save
+        width, height = events.size.value
+        dpi_res = min(width, height) / 10
+        self.fig = sv.Figure((width / dpi_res, height / dpi_res), dpi=dpi_res)
+        self.image = pyglet.image.create(width, height).get_texture()
         self.batch = pyglet.graphics.Batch()
+
+        # Instanziiert klassen
+        self.current_char = sv.CharView()
+
         # dient, um Objekte manuell nach vorne und hinten zu schieben. Je weniger er genutzt wird, umso performanter ist alles.
         # Standardmäßig ist alles im Mittelgrund zwischen Vorder- und Hintergrund
         background = pyglet.graphics.Group(order=-1)
         foreground = pyglet.graphics.Group(order=1)
 
         # Layout für den Statistik-Bildschirm
-        self.background = ui_elements.Sprite("assets/images/StartScreenBackground.png", 0, 0, 100, 100, events, self.batch)
-        self.header = ui_elements.BorderedRectangle("Statistiken", 20, 75, 60, 20, events.color_scheme, color_scheme.Minecraft, 4, events, self.batch)
-        self.current_stats = ui_elements.BorderedRectangle(save_text, 20, 62.5, 60, 10, events.color_scheme, color_scheme.Minecraft, 4, events, self.batch)
-        self.back = ui_elements.InputButton("Zurück", 20, 50, 60, 10, events.color_scheme, color_scheme.Minecraft, 4, events, self.batch)
+        sv.draw_accuracy_chart(self.fig, 98.6, 37.5, self.current_char.get_char())
+        sv.draw_char_pressed_chart(self.fig, 378, 820, self.current_char.get_char())
+        sv.draw_time_per_char_chart(self.fig, 997, 222, self.current_char.get_char())
+        sv.bigu_graphu(self.fig, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], [1, 7.5, 3, 7, 5, 4, 7, 9, 8, 4, 1],
+                       [5, 1, 2, 3, 6.5, 2, 1, 5, 6, 2, 7], self.current_char.get_char())
 
-        # Fängt ab, wenn Buttons gedrückt werden und erzeugt Subscriptions
+        sv.update_image(self.fig, self.image)
+        self.background = ui_elements.Sprite(self.image, 0, 0, 100, 100, events, self.batch)
+        self.header = sv.draw_head_box('Sample', events, self.batch)
+        self.button_back = ui_elements.InputButton("Zurück", 1, 93, 20, 7, events.color_scheme,
+                                                   color_scheme.Minecraft, 7, events, self.batch)
+        self.info_box = ui_elements.BorderedRectangle(save_text, 79, 93, 20, 7,
+                                                      color_scheme.BlackWhite,
+                                                      color_scheme.Minecraft, 7, events, batch=self.batch)
+        self.batch.draw()
+
         from main_controller import PopScreen
-        self._subs.add(self.back.clicked.subscribe(lambda _: self.game_command.on_next(PopScreen())))
+        self._subs.add(events.text.subscribe(self.update_view_on_char_input))
+        self._subs.add(self.button_back.clicked.subscribe(lambda _: self.game_command.on_next(PopScreen())))
+        # Fängt ab, wenn Buttons gedrückt werden und erzeugt Subscriptions
+        # from main_controller import PopScreen
+        # self._subs.add(self.back.clicked.subscribe(lambda _: self.game_command.on_next(PopScreen())))
+
+    def update_view_on_char_input(self, char):
+        self.current_char.change_char(char)
+        print(self.current_char.get_char())
+        self.header.label.text = 'Statistics for the char: ' + self.current_char.get_char()
+        self.fig.clf()
+        last_char_data = sc.get_last_results_of_char(self.game_save, self.current_char.get_char())
+        # graphen zeichnen
+        sv.draw_accuracy_chart(self.fig, round(sc.get_avg_accuracy_for_char(self.game_save, self.current_char.get_char()), 2),
+                               round(last_char_data[4]*100, 2), self.current_char.get_char())
+        sv.draw_char_pressed_chart(self.fig, last_char_data[1], last_char_data[2], self.current_char.get_char())
+        sv.draw_time_per_char_chart(self.fig, round(sc.get_avg_time_for_char(self.game_save, self.current_char.get_char())*1000),
+                                    round(last_char_data[3]*1000), self.current_char.get_char())
+        sv.bigu_graphu(self.fig,
+                       sc.get_count_with_char(self.game_save, self.current_char.get_char()),
+                       sc.get_avg_time_per_char_array(self.game_save, self.current_char.get_char()),
+                       sc.get_time_progression(self.game_save, self.current_char.get_char()),
+                       self.current_char.get_char())
+        sv.update_image(self.fig, self.image)
+        self.batch.draw()
 
     def get_view(self):  # Erzeugt den aktuellen View
         return self.batch
+
+    def get_game_save(self):
+        return self.game_save
--- statistics/statistic_view.py
+++ statistics/statistic_view.py
@@ -12,6 +12,7 @@ from events import Events, Event, Var, Disposable
 import ui_elements
 import color_scheme
 import math
+import main_controller
 
 
 class CharView:
@@ -42,10 +43,14 @@ def roundup_to_10(x):
     return int(math.ceil(x / 10.0)) * 10
 
 
+def roundup_to_1(x):
+    return int(math.ceil(x / 1.0)) * 1
+
+
 def get_highest_value_from_array(sample_array):
-    x = ''
+    x = 0
     for data in sample_array:
-        if x == '':
+        if x == 0:
             x = data
         elif x < data:
             x = data
@@ -54,17 +59,17 @@ def get_highest_value_from_array(sample_array):
 
 def get_yaxis_limit(value1, value2):
 
-    if (value1 > 100) and (value2 > 100):
+    if (value1 <= 1) and (value2 <= 1):
         if value1 > value2:
-            rounded_value = roundup_to_100(value1)
+            rounded_value = roundup_to_1(value1)
             return [rounded_value + 0.1, rounded_value / 10]
         elif value1 < value2:
-            rounded_value = roundup_to_100(value2)
+            rounded_value = roundup_to_1(value2)
             return [rounded_value + 0.1, rounded_value / 10]
         else:
-            rounded_value = roundup_to_100(value1)
+            rounded_value = roundup_to_1(value1)
             return [rounded_value + 0.1, rounded_value / 10]
-    else:
+    elif(value1 <= 10) and (value2 <= 10):
         if value1 > value2:
             rounded_value = roundup_to_10(value1)
             return [rounded_value + 0.1, rounded_value / 10]
@@ -73,6 +78,16 @@ def get_yaxis_limit(value1, value2):
             return [rounded_value + 0.1, rounded_value / 10]
         else:
             rounded_value = roundup_to_10(value1)
+            return [rounded_value + 0.1, rounded_value / 10]
+    else:
+        if value1 > value2:
+            rounded_value = roundup_to_100(value1)
+            return [rounded_value + 0.1, rounded_value / 10]
+        elif value1 < value2:
+            rounded_value = roundup_to_100(value2)
+            return [rounded_value + 0.1, rounded_value / 10]
+        else:
+            rounded_value = roundup_to_100(value1)
             return [rounded_value + 0.1, rounded_value / 10]
 
 
@@ -89,23 +104,23 @@ def render_figure(fig):
     return pyglet.image.ImageData(w, h, "RGBA", data, -4 * w)
 
 
-def update_image(fig):
+def update_image(fig, image):
     # Hier kannst du die gewünschten Änderungen am Bild vornehmen, z.B.:
     image.blit_into(render_figure(fig), 0, 0, 0)
 
 
 def draw_accuracy_chart(fig, avg_accuracy, current_game_accuracy, char_to_analyse):
-    name_of_bars = ['in Average', 'in this Game']
+    name_of_bars = ['Durchschnittlich', 'letztes Spiel']
     accuracy = [avg_accuracy, current_game_accuracy]
     x = np.arange(len(name_of_bars))
     ax = fig.add_subplot(2, 3, (1, 1))
     ax.bar(x, accuracy, color='#34C7FF', width=0.6, align='center')
-    ax.set_title('Accuracy of the Char: ' + char_to_analyse, color='#de8a1b', fontname='Arial', fontsize=17)
-    ax.set_ylabel('Accuracy in %', fontname='Arial', fontsize=17)
+    ax.set_title('Genauigkeit des Char: ' + char_to_analyse, color='#de8a1b', fontname='Arial', fontsize=17)
+    ax.set_ylabel('Genauigkeit in %', fontname='Arial', fontsize=17)
     ax.set_yticks(np.arange(0, 100.1, 10))
     ax.set_xticks(x)
     ax.set_xticklabels(name_of_bars)
-    ax.bar_label(ax.containers[0], label_type='center', color='k')
+    ax.bar_label(ax.containers[0], label_type='center', color='k', fontsize=17)
     ax.xaxis.set_tick_params(labelsize='large')
     ax.yaxis.set_tick_params(labelsize='large')
     ax.set_facecolor('xkcd:black')
@@ -126,18 +141,18 @@ def draw_accuracy_chart(fig, avg_accuracy, current_game_accuracy, char_to_analys
 
 
 def draw_char_pressed_chart(fig, occurance_in_Game, typed_by_user, char_to_analyse):
-    name_of_bars = ['appeared in Game', 'actually pressed']
+    name_of_bars = ['Vorgekommen', 'wirklich gedrückt']
     appearance_of_char = [occurance_in_Game, typed_by_user]
     x = np.arange(len(name_of_bars))
     ax = fig.add_subplot(2, 3, 2)
     ax.bar(x, appearance_of_char, color='#FFC712', width=0.6, align='center')
-    ax.set_title('Appearance of the Char: ' + char_to_analyse, color='#de8a1b', fontname='Arial', fontsize=17)
-    ax.set_ylabel('Count of the Char', fontname='Arial', fontsize=17)
-    ax.set_yticks(np.arange(0, get_yaxis_limit(occurance_in_Game, typed_by_user)[0],
-                            get_yaxis_limit(occurance_in_Game, typed_by_user)[1]))
+    ax.set_title('Häufigkeit Char: ' + char_to_analyse, color='#de8a1b', fontname='Arial', fontsize=17)
+    ax.set_ylabel('Anzahl des Char', fontname='Arial', fontsize=17)
+    ax.set_yticks(np.arange(0, get_yaxis_limit(occurance_in_Game, typed_by_user or 10)[0],
+                            get_yaxis_limit(occurance_in_Game, typed_by_user or 10)[1]))
     ax.set_xticks(x)
     ax.set_xticklabels(name_of_bars)
-    ax.bar_label(ax.containers[0], label_type='center', color='k')
+    ax.bar_label(ax.containers[0], label_type='center', color='k', fontsize=17)
     ax.xaxis.set_tick_params(labelsize='large')
     ax.yaxis.set_tick_params(labelsize='large')
     ax.set_facecolor('xkcd:black')
@@ -158,17 +173,18 @@ def draw_char_pressed_chart(fig, occurance_in_Game, typed_by_user, char_to_analy
 
 
 def draw_time_per_char_chart(fig, avg_time, new_time, char_to_analyse):
-    name_of_bars = ['in average', 'in this Game']
+    name_of_bars = ['Durchschnittlich', 'letztes Spiel']
     appearance_of_char = [avg_time, new_time]
     x = np.arange(len(name_of_bars))
     ax = fig.add_subplot(2, 3, 3)
     ax.bar(x, appearance_of_char, color='#FF55AA', width=0.6, align='center')
-    ax.set_title('Time to press the Char: ' + char_to_analyse, color='#de8a1b', fontname='Arial', fontsize=17)
-    ax.set_ylabel('time in ms', fontname='Arial', fontsize=17)
-    ax.set_yticks(np.arange(0, get_yaxis_limit(avg_time, new_time)[0], get_yaxis_limit(avg_time, new_time)[1]))
+    ax.set_title('Zeit zum drücken des Char: ' + char_to_analyse, color='#de8a1b', fontname='Arial', fontsize=17)
+    ax.set_ylabel('Zeit in ms', fontname='Arial', fontsize=17)
+    ax.set_yticks(np.arange(0, get_yaxis_limit(avg_time, new_time or 100)[0],
+                            get_yaxis_limit(avg_time, new_time or 100)[1]))
     ax.set_xticks(x)
     ax.set_xticklabels(name_of_bars)
-    ax.bar_label(ax.containers[0], label_type='center', color='k')
+    ax.bar_label(ax.containers[0], label_type='center', color='k', fontsize=17)
     ax.set_facecolor('xkcd:black')
     ax.xaxis.label.set_color('yellow')  # setting up X-axis label color to yellow
     ax.yaxis.label.set_color('white')  # setting up Y-axis label color to blue
@@ -187,28 +203,31 @@ def draw_time_per_char_chart(fig, avg_time, new_time, char_to_analyse):
         tick.set_fontsize(15)
 
 
-def bigu_graphu(fig, data_array1, data_array2, data_array3):
-    y = [1, 2, 3, 4, 5, 6,7,8,9,10,11]
+def bigu_graphu(fig, count_array, data_array1, data_array2, char_to_analyse):
+    y = count_array
     data1 = data_array1
     data2 = data_array2
-    data3 = data_array3
     ax = fig.add_subplot(2, 1, 2)
-    ax.plot(y, data1, color='#FF55AA', linestyle='dashed', linewidth=3, marker='o',
-            markerfacecolor='#FF55AA', markersize=12, label="avg Time")
-    ax.plot(y, data2, color='#34C7FF', linestyle='dashed', linewidth=3, marker='o',
-            markerfacecolor='#34C7FF', markersize=12, label="avg Accuracy")
-    ax.plot(y, data3, color='#FFC712', linestyle='dashed', linewidth=3, marker='o',
-            markerfacecolor='#FFC712', markersize=12, label="chars typed")
-    ax.set_title('Stats for all Chars', color='#de8a1b', fontname='Arial', fontsize=17)
-    ax.set_xlabel('Characters', fontname='Arial', fontsize=17)
-    ax.set_ylabel('Values', fontname='Arial', fontsize=17)
-    ax.set_yticks(np.arange(0, get_yaxis_limit(get_highest_value_from_array(data1 + data2 + data3), 0)[0],
-                  get_yaxis_limit(get_highest_value_from_array(data1 + data2 + data3), 0)[1]))
+    if len(y) < 6:
+        ax.plot(y, data1, color='#FF55AA', linestyle='dashed', linewidth=3, marker='o',
+                markerfacecolor='#FF55AA', markersize=15, label="Ø Zeit")
+        ax.plot(y, data2, color='#34C7FF', linestyle='dashed', linewidth=3, marker='o',
+                markerfacecolor='#34C7FF', markersize=15, label="Ø Genauigkeit")
+    else:
+        ax.plot(y, data1, color='#FF55AA', linestyle='dashed', linewidth=3, marker='o',
+                markerfacecolor='#FF55AA', markersize=1, label="Ø Zeit")
+        ax.plot(y, data2, color='#34C7FF', linestyle='dashed', linewidth=3, marker='o',
+                markerfacecolor='#34C7FF', markersize=1, label="Ø Genauigkeit")
+    ax.set_title('Zeitlicher Fortschritt des Char: ' + char_to_analyse, color='#de8a1b', fontname='Arial', fontsize=17)
+    ax.set_xlabel('Anzahl der Spiele: ' + str(len(y)), fontname='Arial', fontsize=17)#'Characters'
+    ax.set_ylabel('Zeit in ms', fontname='Arial', fontsize=17)
+    ax.set_yticks(np.arange(0, get_yaxis_limit(get_highest_value_from_array(data1 + data2), 0)[0] or 100,
+                  get_yaxis_limit(get_highest_value_from_array(data1 + data2), 0)[1] or 100))
     ax.set_xticks(np.arange(0, len(y)+2.1))
     # Text an dem Graphen
     # ax.text(y[-1]+0.1, data1[-1]-0.2, 'December', style='normal', color='green', fontsize=17)
 
-    ax.legend(frameon=False, framealpha=0.8, bbox_to_anchor=(1.02, 1), labelcolor='linecolor', fontsize=15, loc=1)
+    ax.legend(frameon=False, framealpha=0.8, bbox_to_anchor=(1.05, 1), labelcolor='linecolor', fontsize=15, loc=1)
     # ncol = 2,facecolor='k', labelcolor='w'
 
     ax.set_facecolor('xkcd:black')
@@ -217,7 +236,7 @@ def bigu_graphu(fig, data_array1, data_array2, data_array3):
 
     ax.xaxis.set_tick_params(labelsize='large')
     ax.yaxis.set_tick_params(labelsize='large')
-    #ax.tick_params(axis='x', colors='#CC5BA4', width=3)  # setting up X-axis tick color to red
+    ax.tick_params(axis='x', colors='#A4A4A4', width=3)  # setting up X-axis tick color to red
     ax.tick_params(axis='y', colors='#A4A4A4', width=3)  # setting up Y-axis tick color to black
     ax.spines['left'].set_color('#646464')  # setting up Y-axis tick color to red
     ax.spines['bottom'].set_color('#646464')  # setting up above X-axis tick color to red
@@ -231,64 +250,7 @@ def bigu_graphu(fig, data_array1, data_array2, data_array3):
         tick.set_fontsize(15)
 
 
-def draw_head_box(display_char, batch):
-    return ui_elements.BorderedRectangle('Statistics for the char: ' + display_char, 35, 93, 30, 7,
+def draw_head_box(display_char, events, batch):
+    return ui_elements.BorderedRectangle('Statistics for the char: ' + display_char, 27, 93, 46, 7,
                                          color_scheme.BlackWhite,
-                                         color_scheme.Minecraft, 4, events, batch=batch)
-
-
-# Setting up windows using Pyglet
-window = pyglet.window.Window(width=1280, height=720)
-dpi_res = min(window.width, window.height) / 10
-fig = Figure((window.width / dpi_res, window.height / dpi_res), dpi=dpi_res)
-batch = pyglet.graphics.Batch()
-canvas = {}
-image = pyglet.image.create(width=1280, height=720).get_texture()
-events = Events(
-    size=Var((window.width, window.height))
-)
-
-# Instanziiert klassen
-current_char = CharView()
-current_game_save = GameSaveInfo()
-
-# Zeichnet erste graphen
-draw_accuracy_chart(fig, 98.6, 37.5, current_char.get_char())
-draw_char_pressed_chart(fig, 378, 820, current_char.get_char())
-draw_time_per_char_chart(fig, 997, 222, current_char.get_char())
-bigu_graphu(fig,[1, 7.5, 3, 7, 5, 4, 7, 9, 8, 4, 1], [5, 1, 2, 3, 6.5, 2,1,5,6,2,7], [7.7, 2, 3, 6, 7.9, 0.5,1,2,1,1,3])
-
-update_image(fig)
-center_line = shapes.Line(1280 * 0, 720 * 0.46, 1280 * 1, 720 * 0.46, 5, color=(100, 100, 100), batch=batch)
-
-# x,y,w,h in % der gesamten Bildschirmgröße. Schriftgröße einfach testen, funktioniert nur mit Integer :(
-canvas[1] = draw_head_box('Sample', batch)
-
-
-# Hier ist noch ein fehler D:
-@window.event
-def on_draw():
-    window.clear()
-    image.blit(0, 0)
-    batch.draw()
-
-
-@window.event
-def on_text(text):
-    current_char.change_char(text)
-    print(current_char.get_char())
-    canvas[1].label.text = 'Statistics for the char: ' + current_char.get_char()
-    fig.clf()
-    # graphen zeichnen
-    draw_accuracy_chart(fig, 98.6, 37.5, current_char.get_char())
-    draw_char_pressed_chart(fig, 378, 820, current_char.get_char())
-    draw_time_per_char_chart(fig, 1997, 222, current_char.get_char())
-    bigu_graphu(fig,
-                [1, 7.5, 3, 7, 5, 4, 1, 7, 3, 2.5, 5],
-                [5, 1, 2, 3, 6.5, 2, 9, 1.1, 5, 35, 5],
-                [7.7, 2, 3, 6, 7.9, 0.5, 3, 4, 5, 7, 2])
-    update_image(fig)
-
-
-# Runner code
-pyglet.app.run()
+                                         color_scheme.Minecraft, 3, events, batch=batch)
