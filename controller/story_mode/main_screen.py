e52762431aa1e32e3cf9724fa745571b2b4aab56
Janek Engel <loewe34@gmx.net>
2023-06-04 11:12:47 +0200

    Level4 und 5 als WIP gekennzeichnet

--- controller/story_mode/main_screen.py
+++ controller/story_mode/main_screen.py
@@ -23,23 +23,25 @@ class MainStoryScreen(Screen):
         foreground = pyglet.graphics.Group(order=1)
 
         # Layout für den Startbildschirm des Lern-Modus
-        self.background = ui_elements.Sprite("assets/images/StartScreenBackground.png", 0, 0, 100, 100, events, self.batch)
+        self.background = ui_elements.Sprite("assets/images/StartScreenBackground.png", 0, 0, 100, 100, events, self.batch, background)
         self.header = ui_elements.BorderedRectangle("Story", 20, 75, 60, 20, events.color_scheme, color_scheme.Minecraft, 4, events, self.batch)
 
         self.back = ui_elements.InputButton("Zurück", 40, 10, 20, 10, events.color_scheme, color_scheme.Minecraft, 10, events, self.batch)
         self.settings = ui_elements.InputButton("Einstellungen", 2.5, 85, 12.5, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch)
         self.statistics = ui_elements.InputButton("Statistiken", 85, 85, 12.5, 10, events.color_scheme, color_scheme.Minecraft, 8.4, events, self.batch)
 
-        self.level1_button = ui_elements.BorderedSpriteButton("assets/images/port.gif", 2.5, 45, 15, 15, events.color_scheme, events, self.batch, foreground)
-        self.level1_label = ui_elements.InputButton("Erstes Level", 2.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch, foreground)
-        self.level2_button = ui_elements.BorderedSpriteButton("assets/images/forest.gif", 22.5, 45, 15, 15, events.color_scheme, events, self.batch, foreground)
-        self.level2_label = ui_elements.InputButton("Zweites Level", 22.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch, foreground)
-        self.level3_button = ui_elements.BorderedSpriteButton("assets/images/city.gif", 42.5, 45, 15, 15, events.color_scheme, events, self.batch, foreground)
-        self.level3_label = ui_elements.InputButton("Drittes Level", 42.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch, foreground)
-        self.level4_button = ui_elements.BorderedSpriteButton("assets/images/bridge.gif", 62.5, 45, 15, 15, events.color_scheme, events, self.batch, foreground)
-        self.level4_label = ui_elements.InputButton("Viertes Level", 62.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch, foreground)
-        self.level5_button = ui_elements.BorderedSpriteButton("assets/images/city.gif", 82.5, 45, 15, 15, events.color_scheme, events, self.batch, foreground)
-        self.level5_label = ui_elements.InputButton("Fünftes Level", 82.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch, foreground)
+        self.level1_button = ui_elements.BorderedSpriteButton("assets/images/port.gif", 2.5, 45, 15, 15, events.color_scheme, events, self.batch)
+        self.level1_label = ui_elements.InputButton("Erstes Level", 2.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch)
+        self.level2_button = ui_elements.BorderedSpriteButton("assets/images/forest.gif", 22.5, 45, 15, 15, events.color_scheme, events, self.batch)
+        self.level2_label = ui_elements.InputButton("Zweites Level", 22.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch)
+        self.level3_button = ui_elements.BorderedSpriteButton("assets/images/city.gif", 42.5, 45, 15, 15, events.color_scheme, events, self.batch)
+        self.level3_label = ui_elements.InputButton("Drittes Level", 42.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch)
+
+        self.under_development = ui_elements.BorderedRectangle("In der Entwicklung", 65, 35, 30, 22.5, events.color_scheme, color_scheme.Minecraft, 4.5, events, self.batch, foreground)
+        self.level4_button = ui_elements.BorderedSpriteButton("assets/images/bridge.gif", 62.5, 45, 15, 15, events.color_scheme, events, self.batch)
+        self.level4_label = ui_elements.InputButton("Viertes Level", 62.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch)
+        self.level5_button = ui_elements.BorderedSpriteButton("assets/images/city.gif", 82.5, 45, 15, 15, events.color_scheme, events, self.batch)
+        self.level5_label = ui_elements.InputButton("Fünftes Level", 82.5, 32.5, 15, 10, events.color_scheme, color_scheme.Minecraft, 8, events, self.batch)
 
         # Fängt ab, wenn Buttons gedrückt werden und erzeugt Subscriptions
         self._subs.add(self.back.clicked.subscribe(lambda _: self.go_back()))
@@ -58,8 +60,8 @@ class MainStoryScreen(Screen):
         self._subs.add(self.level2_label.clicked.subscribe(lambda _: self.reload_screen(Level2Screen.init_fn(save))))
         self._subs.add(self.level3_button.clicked.subscribe(lambda _: self.reload_screen(Level3Screen.init_fn(save))))
         self._subs.add(self.level3_label.clicked.subscribe(lambda _: self.reload_screen(Level3Screen.init_fn(save))))
-        self._subs.add(self.level4_button.clicked.subscribe(lambda _: self.reload_screen(Level4Screen.init_fn(save))))
-        self._subs.add(self.level4_label.clicked.subscribe(lambda _: self.reload_screen(Level4Screen.init_fn(save))))
+        # self._subs.add(self.level4_button.clicked.subscribe(lambda _: self.reload_screen(Level4Screen.init_fn(save))))
+        # self._subs.add(self.level4_label.clicked.subscribe(lambda _: self.reload_screen(Level4Screen.init_fn(save))))
         # self._subs.add(self.level5_button.clicked.subscribe(lambda _: self.reload_screen(Level5Screen.init_fn())))
         # self._subs.add(self.level5_label.clicked.subscribe(lambda _: self.reload_screen(Level5Screen.init_fn())))
 
